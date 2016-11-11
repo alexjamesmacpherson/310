3813098964fc542a4f13bc279ae3a85da14b5c8c
Alex Macpherson <alexjamesmacpherson@gmail.com>
2016-11-11 01:37:31 +0000

    Secure user sessions & cookies with basic sign-up and login forms.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root 'static#home'
 
   get '/about' => 'static#about'
-  get '/login' => 'static#login'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :users
 end
